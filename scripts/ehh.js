46b4937887fa2e8b80316decca80160ab6b35145
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-09 18:11:28 +0530

    u

--- scripts/ehh.js
+++ scripts/ehh.js
@@ -2,9 +2,7 @@
 window.onload = windOnLoad();
 
 function windOnLoad() {
-
     //window storage == session storage
     console.log("ehh is running! on >>>", window.document.title,window.document.location);
-
 }
 
