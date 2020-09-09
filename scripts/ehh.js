1125cafcc8fd4b2a1411b42f9175c1bcc74c4963
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-09 18:40:54 +0530

    u

--- scripts/ehh.js
+++ scripts/ehh.js
@@ -3,6 +3,67 @@ window.onload = windOnLoad();
 
 function windOnLoad() {
     //window storage == session storage
-    console.log("ehh is running! on >>>", window.document.title,window.document.location);
+    console.log("ehh is running! on >>>", window.document.title, window.document.location.origin);
+    var listeners = createListeners(this);
 }
 
+function createListeners(entity) {
+ //   console.log(entity);
+    var events = find(entity, 'on');
+  // console.log("events Found",events);
+   var a = events.forEach(create);
+    console.log(a);
+    save(events, this.constructor.name+"listeners");
+    console.log("listernes created & Saved to local storagea at ", new Date().toLocaleString().replace(',', ''), this.constructor.name);
+}
+
+let event = {
+    "origin": ["mouse", "window", "ehh", "HTTP", "keyBoard"],
+    "eventState": {
+        "currentState": "mouseMove",
+        "previousState": "mouseDown",
+        "predictNextState": ""
+    }
+
+
+}
+
+function onEvent(e) {
+ 
+    if (e.type === "mousedown") {
+        console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
+        // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
+    } else if (e.type === "contextmenu") {
+        console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
+        //createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
+        e.preventDefault();
+    } else {
+        if (e.type === "mouseover") {
+            console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
+            // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
+        } 
+
+
+    }
+
+}
+
+
+function create(entity) {
+    window[entity] = onEvent;
+}
+
+
+
+function find(entity, keyTofind) {
+   // console.log("finding", keyTofind, "in", entity);
+    var result = Object.keys(entity).filter(function (key, index, self) {
+        return !key.indexOf(keyTofind);
+    });
+    return result;
+}
+
+function save(entity, keyTitle) {
+     console.log("saving", keyTitle, JSON.stringify(entity));
+    window.localStorage.setItem(keyTitle, JSON.stringify(entity));
+}
