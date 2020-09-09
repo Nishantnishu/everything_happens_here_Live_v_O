d3f911e32d24351bcc0697a9e27e60f770153b08
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-09 18:07:44 +0530

    u

---
+++ scripts/ehh.js
@@ -0,0 +1,10 @@
+
+window.onload = windOnLoad();
+
+function windOnLoad() {
+
+    //window storage == session storage
+    console.log("ehh is running! on >>>", window.document.title,window.document.location);
+
+}
+
--- scripts/tobeimplemented.js/links.js
+++ scripts/tobeimplemented.js/links.js
@@ -6,6 +6,15 @@
 //https://codepen.io/iceable/pen/yLBrZOd
 //https://codepen.io/argyleink/pen/bgVOMO
 //https://codepen.io/anthonydugois/pen/RpYBmy
+//https://codepen.io/sol0mka/pen/ogOYJj
+//https://codepen.io/dudleystorey/pen/HkwBo
+//https://codepen.io/CBeghin/pen/HeuiF
+//https://themer.dev/?colors.dark.accent0=%23CA3E5A&colors.dark.accent1=%23D8843E&colors.dark.accent2=%23EBB062&colors.dark.accent3=%2381A559&colors.dark.accent4=%2342ABAB&colors.dark.accent5=%234496CD&colors.dark.accent6=%239770B2&colors.dark.accent7=%23B35D8D&colors.dark.shade0=%2313222E&colors.dark.shade7=%23ACBECC&activeColorSet=dark&calculateIntermediaryShades.dark=true&calculateIntermediaryShades.light=true
+//https://timothylong.com/
+//https://codepen.io/anthonydugois/pen/RpYBmy
+//https://github.com/ilyashubin/hover-inspect/blob/master/app/hoverinspect.js
+//https://quilljs.com/docs/quickstart/
+//https://kothing.github.io/editor/html/getting-started.html
 
 
 buttons a: hover: before {
