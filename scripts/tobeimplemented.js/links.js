b0fc7ceff9392c19a411b01de8a6f0ad2986e9f8
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-09 13:47:58 +0530

    p

--- scripts/tobeimplemented.js/links.js
+++ scripts/tobeimplemented.js/links.js
@@ -2,3 +2,5 @@
 //https://codepen.io/borntofrappe/pen/bGbYmwp
 //https://codepen.io/zitrusfrisch/pen/wvcxy
 //https://codepen.io/calvinalx/pen/aOZNwj
+//https://codepen.io/P1N2O/pen/pyBNzX
+//https://codepen.io/iceable/pen/yLBrZOd
