518d89a5f3cf9ce28a1732f3a3747080312f587a
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-09-10 18:43:51 +0530

    u

--- scripts/ehh.js
+++ scripts/ehh.js
@@ -13,32 +13,63 @@ function createListeners(entity) {
   // console.log("events Found",events);
     var a = events.forEach(create);
     console.log(a);
-    save(events, this.constructor.name+"listeners");
+    save(events, this.constructor.name + "listeners");
+    
     console.log("listernes created & Saved to local storagea at ", new Date().toLocaleString().replace(',', ''), this.constructor.name);
 }
 let event = {
     "origin": ["mouse", "window", "ehh", "HTTP", "keyBoard"],
-    
-
-
 }
 
+function initState(e) {
+    var nodes = [];
+    //currentState = e.type;
+    document.documentElement.querySelectorAll('*').forEach(function (node) {
+        node.setAttribute("currentstate", "inDom"); node.setAttribute("prevstate", "");
+       //  console.log(node);
+    });
+    
+}
+
+function changeState(e) {
+    var targetElement = e.target;
+    console.log(targetElement);
+    let currentState = targetElement.getAttribute('currentstate');
+    let prevState = targetElement.getAttribute('prevstate'); 
+    
+    console.log("current state", currentState);
+    console.log("prev state",prevState);
+    if (prevState != currentState) {
+        targetElement.setAttribute('prevstate', currentState); //console.log(prevState);
+        targetElement.setAttribute('currentstate', e.type); //console.log(prevState);
+        console.log("statechanged");
+    }
+
+    //console.log(targetElement.getAttributes(prevstate));
+
+}
 //this function acts like a event conductor, read it's event command mapp from a json file, which mapps 
 //Ignore Events from Json to be implemented
 //https://github.com/philipwalton/router/blob/master/index.js
 
 function onEvent(e) {
-    //console.log(e);
+  //  console.log(e.constructor.name, e.type, "captured", e.target.tagName);
+    if (e.type === "pageshow") {
+        console.log(e.constructor.name, e.type, "captured", e.target.tagName);
+       initState(e);
+        // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
+    }   
     if (e.type === "mouseover") {
-          console.log(e.constructor.name, e.type, "captured", e.target.tagName);
-          //updateAttr(e);
+         console.log(e.constructor.name, e.type, "captured", e.target.tagName);
+        changeState(e);
           // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
       }  if (e.type === "click") {
-        console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
-      click(e);
-        // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
-    }  if (e.type === "mousedown") {
        // console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
+     // click(e);
+        // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
+    } if (e.type === "mousedown") {
+        changeState(e);
+       console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
         // createElement(e); // onmousedown(e); // onmousedown(e); // console.log("body");
     }  if (e.type === "contextmenu") {
        // console.log(e.constructor.name, e.type, "captured", e.target.constructor.name);
@@ -71,7 +102,6 @@ function rightClick(event) {
     contextElement.style.display = 'block';
 }
 
-
 function click(e) { 
     document.getElementById("context-menu").classList.remove("active");
     var contextElement = document.getElementById("context-menu");
@@ -79,9 +109,15 @@ function click(e) {
     contextElement.style.display = 'none';
 }
 
+
+
+
 function updateAttr(e){
 var targetElement = e.target;
-targetElement.setAttribute('class', e.type);
+//targetElement.setAttribute('class', e.type);
 console.log(targetElement);
 //targetElement.classList.add(e.type);
 }
+
+
+
--- scripts/ehhStates.js
+++ scripts/ehhStates.js
@@ -1,5 +1,49 @@
-const eventState: {
-    currentState: "mouseMove",
-        "previousState": "mouseDown",
-            "predictNextState": ""
+const ignoreEvent = ['PointerEvent'];
+const autoSaveTimer = "10 Sec";
+const ehhNavigation = ["#hashChange", "elementChildOpen"]
+const elementStates = ["pinned", "saved", "mouseOver/hover", "editable", "draggable", "resizeable", "contenteditable", "inDocument", "hidden"];
+const ehhType = ["client", "server", "clientSideServer"];
+const ehhClientType = ["addon", "browserSnippet", "extension", "browserApp", "webApp/webSite", "destopApp"];
+const ehhDevices = ["mac", "pc", "ios", "andriod"]
+const ehhModes = ["editorMode", "designMode", "developmentMode"]
+const elementAttributes = ["form", "function", "beauty"];
+const entityState = [prevState, currentState, nextState];
+const stateService = {
+
+
+
+}
+
+
+
+
+var state = {
+    currentState: "",
+    previousState:""
+
+}
+
+var event = {
+    "origin": ["mouse", "window", "ehh", "HTTP", "keyBoard"],
+
+
+}
+
+function changeState(e) {
+    var targetElement = e.target;
+    console.log(targetElement);
+    targetElement.setAttribute('currentState', e.type);
+    if (targetElement.state) {
+        var currentState = targetElement.getAttributes(state);
+        console.log(currentState);
+    }
+}
+
+
+changeState(entity,newState) {
+    //validate that newState actually exists
+    entity.getAttributes(currentState);
+    entity.setAtt
+    console.log(targetElement);
+//targetElement.classList.add(e.type);
 }
